4f05e9a041e152253d362556fcb1909aa9cfc039
Radek Szczesny <radek.szczesny@pearson.com>
2023-10-17 10:23:36 +0200

    Basic

--- script.py
+++ script.py
@@ -15,16 +15,19 @@ while choice != 5:
     index = 0
     
     if choice == 1:
+        list.clear()
+        list.append("Learning Python")
+        list.append("Learning Terraform")
         print()
-        print("Its your up to date task list:")
+        print("Its your up to date tasks list:")
         print()
-        file = open("Day.txt" , "r")
+        file = open("Day.txt" , "r") ## "r" - reading
         for line in file.readlines():
-            list.append(line.strip())
+            list.append(line.strip()) ## strip - avoid unnecessary characters
+        file.close
         for task in list:   
             print(str(index) + " " + task) 
             index += 1
-        file.close
         print()
             
     elif choice == 2:
@@ -42,22 +45,39 @@ while choice != 5:
     elif choice == 3:
         print()
         remove_task = int(input("Provide a number of task that can be remove from the list: "))
-        list.pop(remove_task)
-        print()
-        print("Its your up to date task list:")
-        print()
-        for task in list:
-            print(str(index) + " " + task)     ##print task
-            index += 1
-        print()
+        if remove_task == 0:
+            print()
+            print("Important: You cannot remove task 'Learning Python' !!! It's so much important for you")
+            print("Select other option or try to remove other task")
+            print()
+        elif remove_task == 1:
+            print()
+            print("Important: You cannot remove task 'Learning Terraform' !!! It's so much important for you")
+            print("Select other option or try to remove other task")
+            print()
+        else:
+            list.pop(remove_task)
+            print()
+            print("Its your up to date task list:")
+            print()
+            for task in list:
+                print(str(index) + " " + task)     ##print task
+                index += 1
+            print()
+            
+            
         
     elif choice == 4:
-        file = open("Day.txt","w+")
+        #remove_mandatory = ["Learning Python" , "Learning Terraform"]
+        list.pop(0)
+        list.pop(0)
+        file = open("Day.txt","w+") ## "w" - write
         for task in list:
-            file.write(task + "\n") 
-            index += 1
+            file.write(task + "\n") ## "\n" - with new line
         file.close
-                    
+        print("Saved to file: Day.txt")
+     
+        
     elif choice == 5:
         print()
         print("Good job! See you next time")   
