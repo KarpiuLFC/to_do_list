f816780427e7a85a4eb05271b060aad830cfdd3b
Radek Szczesny <radek.szczesny@pearson.com>
2023-10-18 09:02:43 +0200

    merge

--- script.py
+++ script.py
@@ -96,83 +96,6 @@ while choice != 6:
                 print("Incorrect value has been provided! Please start your job again")
             print()
             
-<<<<<<< HEAD
-            
-        list = []
-list.append("Learning Python")
-list.append("Learning Terraform")
-
-choice = 0
-while choice != 6:
-    print("======================================================================") 
-    print("1. Show all your tasks")
-    print("2. Add task")
-    print("3. Remove task")
-    print("4. Save changes")
-    print("5. Export to file")
-    print("6. Exit") 
-    print("======================================================================")  
-    choice = int(input("Choose your option [1-6]: "))
-    index = 0
-    
-    if choice == 1:
-        list.clear()
-        list.append("Learning Python")
-        list.append("Learning Terraform")
-        print()
-        print("Its your up to date tasks list:")
-        print()
-        file = open("Day.txt" , "r") ## "r" - reading
-        for line in file.readlines():
-            list.append(line.strip()) ## strip - avoid unnecessary characters
-        file.close
-        for task in list:   
-            print(str(index) + " " + task) 
-            index += 1
-        print()
-            
-    elif choice == 2:
-        print()
-        new_task = input("Insert new task which will be added to the list: ")
-        print()
-        print("Its your up to date task list:")
-        print()
-        list.append(new_task)
-        for task in list:   
-            print(str(index) + " " + task)     ##print task
-            index += 1
-        print()
-           
-    elif choice == 3:
-        print()
-        for task in list:   
-            print(str(index) + " " + task) 
-            index += 1
-        print()
-        remove_task = int(input("Provide a number of task that can be remove from the list: "))
-        if remove_task == 0:
-            print()
-            print("Important: You cannot remove task 'Learning Python' !!! It's so much important for you")
-            print("Select other option or try to remove other task")
-            print()
-        elif remove_task == 1:
-            print()
-            print("Important: You cannot remove task 'Learning Terraform' !!! It's so much important for you")
-            print("Select other option or try to remove other task")
-            print()
-        else:
-            list.pop(remove_task)
-            index = 0
-            print()
-            print("Its your up to date task list:")
-            print()
-            for task in list:
-                print(str(index) + " " + task)     ##print task
-                index += 1
-            print()
-            
-=======
->>>>>>> update_list
     elif choice == 4:
         print()
         #remove_mandatory = ["Learning Python" , "Learning Terraform"] - its always there in the list
@@ -206,14 +129,3 @@ while choice != 6:
         print("Select number again in range [1-6]!")
         print()
         
-<<<<<<< HEAD
-    
-    
-
-        
-    
-
-    
-
-=======
->>>>>>> update_list
